c49d8309b704de720480711d87be797323a0e230
danjack <133123813+biondodaniel@users.noreply.github.com>
2025-04-16 11:04:48 +0200

    Update tpm.c - update mapping

--- core/kernel/tpm.c
+++ core/kernel/tpm.c
@@ -143,8 +143,11 @@ void tpm_map_log_area(void *fdt)
 
 	rounded_size = ROUNDUP(tpm_log_size, SMALL_PAGE_SIZE);
 
-	tpm_log_addr = core_mmu_add_mapping(MEM_AREA_RAM_SEC, log_addr,
-					    rounded_size);
+	//tpm_log_addr = core_mmu_add_mapping(MEM_AREA_RAM_SEC, log_addr,
+	//				    rounded_size);
+	
+	tpm_log_addr = core_mmu_add_mapping(MEM_AREA_NSEC_SHM, log_addr, rounded_size);
+
 	if (!tpm_log_addr) {
 		EMSG("TPM: Failed to map TPM log memory");
 		return;
